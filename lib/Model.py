42b2b71d9802953eef55b9ab164034953e8ee0d1
Karan Singhal <karan1149@gmail.com>
2018-06-02 13:24:38 -0700

    Model interface abstracting away vectorization and NNs

---
+++ lib/Model.py
@@ -0,0 +1,16 @@
+# Interface for model that does binary classification
+# on text data. Abstracts away vectorization and model
+# type
+class Model(object):
+	def __init__(self):
+		pass
+
+	# X is a list of text strings and y is a corresponding list 
+	# of 0 or 1 labels
+	def fit(X, y):
+		pass
+
+	# Run prediction on a single text string x, return some value between
+	# 0 and 1 representing the model's confidence that x is a positive value
+	def predict(x):
+		pass
