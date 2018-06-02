2babefdbbc96a1ac434fa2717276b70739a9b763
Karan Singhal <karan1149@gmail.com>
2018-06-02 15:01:41 -0700

    Model is just an interface

--- lib/Model.py
+++ lib/Model.py
@@ -15,4 +15,4 @@ class Model(object):
 	# Run prediction on a single text string x, return some value between
 	# 0 and 1 representing the model's confidence that x is a positive value
 	def predict(self, item):
-		return random.uniform(0,1)
+		pass
