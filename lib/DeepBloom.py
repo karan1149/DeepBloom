006215618fccfbafaf3f0e1cd0d43226f1df3a17
Karan Singhal <karan1149@gmail.com>
2018-06-02 14:45:05 -0700

    Minor deepbloom fixes

--- lib/DeepBloom.py
+++ lib/DeepBloom.py
@@ -6,7 +6,7 @@ class DeepBloom(object):
     def __init__(self, model, data, fp_rate):
         self.model = model
         self.threshold = 0
-        self.fp_rate = fp_rate
+        self.fp_rate = float(fp_rate)
         self.data = data
         self.train()
         self.bloomFilter = None
@@ -28,20 +28,19 @@ class DeepBloom(object):
         shuffled = self.shuffle_for_training(s1, self.data.positives)
         self.model.fit(shuffled[0], shuffled[1])
 
-        ## We want a threshold such that s2.size/2 elements
+        ## We want a threshold such that at most s2.size * fp_rate/2 elements
         ## are greater than threshold.
-        fp_index = int((len(s2) * self.fp_rate/2) + 1)
+        fp_index = int(math.ceil((len(s2) * self.fp_rate/2)))
         predictions = [self.model.predict(item) for item in s2]
         list.sort(predictions)
-        threshold = predictions[fp_index]
-        print(threshold)
+        self.threshold = predictions[fp_index]
 
 
     def split_negatives(self):
         size = len(self.data.negatives)
         s1 = self.data.negatives[0:math.floor(.8*size)]
-        s2 = self.data.negatives[math.ceil(.8*size):math.floor(.9*size)]
-        s3 = self.data.negatives[math.ceil(.9*size):]
+        s2 = self.data.negatives[math.floor(.8*size):math.floor(.9*size)]
+        s3 = self.data.negatives[math.floor(.9*size):]
         return (s1, s2, s3)
 
     def shuffle_for_training(self, negatives, positives):
