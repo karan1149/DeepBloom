7125327640fc282fee4c3be52951b8f4aeab9025
Philip Weiss <weissp68@stanford.edu>
2018-06-02 14:56:21 -0700

    add bloom filter to deepbloom

--- lib/DeepBloom.py
+++ lib/DeepBloom.py
@@ -9,12 +9,23 @@ class DeepBloom(object):
         self.fp_rate = fp_rate
         self.data = data
         self.train()
-        self.bloomFilter = None
+        self.createBloomFilter()
 
     def check(self, item):
         if self.model.predict(item) > self.threshold:
             return true
         return self.bloomFilter.check(item)
+
+    def createBloomFilter(self):
+        self.bloomFilter = BloomFilter(
+            len(self.data.positives),
+            .001,
+            self.string_digest
+        )
+        for positive in self.data.positives:
+            if not self.model.predict(positive):
+                self.bloomFilter.add(positive)
+
 
 
     ## For now, only train the first model.
@@ -26,11 +37,11 @@ class DeepBloom(object):
         ## Shuffle together subset of negatives and positives.
         ## Then, train the model on this data.
         shuffled = self.shuffle_for_training(s1, self.data.positives)
-        self.model.fit(shuffled[0], shuffled[1])
+        self.model.fit(list(shuffled[0]), list(shuffled[1]))
 
-        ## We want a threshold such that s2.size/2 elements
-        ## are greater than threshold.
-        fp_index = int((len(s2) * self.fp_rate/2) + 1)
+        ## We want a threshold such that fp_rate/2 elements
+        ## in s2 are greater than threshold.
+        fp_index = len(s2) - int((len(s2) * self.fp_rate/2))
         predictions = [self.model.predict(item) for item in s2]
         list.sort(predictions)
         threshold = predictions[fp_index]
@@ -49,4 +60,7 @@ class DeepBloom(object):
         b = [(i, 1) for i in positives]
         combined = a + b
         random.shuffle(combined)
-        return combined
+        return list(zip(*combined))
+
+    def string_digest(item, index):
+        return mmh3.hash(bytes(item, 'utf-8'), index)
--- lib/DeepBloomTest.py
+++ lib/DeepBloomTest.py
@@ -1,8 +1,14 @@
 from DeepBloom import DeepBloom
 from Model import Model
+import random
+import string
 
 class Data(object):
     def __init__(self):
-        self.negatives = [i for i in range(100000)]
-        self.positives = [i for i in range(100001,200001)]
+        self.negatives = [self.generate_random_string(20) for i in range(10000)]
+        self.positives = [self.generate_random_string(20) for i in range(10001,20001)]
+
+    def generate_random_string(self, N):
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+
 db = DeepBloom(Model(), Data(), .05)
