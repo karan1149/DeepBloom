5733623cc9618b19c15ab602e1f6a00c8e2fbf15
Karan Singhal <karan1149@gmail.com>
2018-06-02 19:44:02 -0700

    Added vectorization to GRU model

--- lib/GRUModel.py
+++ lib/GRUModel.py
@@ -1,11 +1,14 @@
 from Model import Model
+import utils
 
 class GRUModel(Model):
-	def __init__(self):
-		pass
+	def __init__(self, embeddings_path, embedding_dim, lr):
+		self.embeddings_path = embeddings_path
+		self.embedding_dim = embedding_dim
+		self.lr = lr
 
 	def fit(self, X, y):
-		pass
+		X, y = vectorize_dataset(X, y)
 
 	def predict(self, x):
 		pass
