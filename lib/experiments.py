8166b6a3fefb990f3d69a8741db79b3a59294b0c
Karan Singhal <karan1149@gmail.com>
2018-06-03 13:08:19 -0700

    Added experiments file

---
+++ lib/experiments.py
@@ -0,0 +1,14 @@
+from test_models import test_gru_model
+
+if __name__=='__main__':
+	with open('../data/dataset.json', 'r') as f:
+	    dataset = json.load(f)
+
+	positives = dataset['positives']
+	negatives = dataset['negatives']
+
+	print("Data fraction 0.5, fp_rate 0.01, pca_embedding_dim 16")
+	test_gru_model(positives, negatives, data_fraction=0.5, fp_rate=0.01, pca_embedding_dim=16, maxlen=50, gru_size=16)
+
+	print("Data fraction 0.5, fp_rate 0.01, pca_embedding_dim None")
+	test_gru_model(positives, negatives, data_fraction=0.5, fp_rate=0.01, pca_embedding_dim=None, maxlen=50, gru_size=16)
