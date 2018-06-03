395756c6221f31fe245eca5ec4a17894551aecc6
Karan Singhal <karan1149@gmail.com>
2018-06-02 19:43:36 -0700

    Test deep bloom fixes

--- lib/test_deep_bloom.py
+++ lib/test_deep_bloom.py
@@ -13,7 +13,7 @@ class Data(object):
 
 class TestDeepBloom(object):
     def test_almost_perfect_model(self):
-        fp_rate = .20
+        fp_rate = .05
 
         ## Get data to train on
         with open('../data/dataset.json', 'r') as f:
@@ -21,20 +21,32 @@ class TestDeepBloom(object):
 
         positives = dataset['positives']
         negatives = dataset['negatives']
-        data = Data(positives, negatives)
 
-        db = DeepBloom(AlmostPerfectModel(fp_rate), data, fp_rate)
+        train_dev_negatives = negatives[:int(.90 * len(negatives))]
+        test_negatives = negatives[int(.90 * len(negatives)):]
+        print("Number train, dev", len(train_dev_negatives))
+        print("Number test", len(test_negatives))
+        data = Data(positives, train_dev_negatives)
+
+        # this parameter is not related to fp_rate
+        db = DeepBloom(AlmostPerfectModel(.2), data, fp_rate)
         false_positives = 0.0
-        false_negatives = 0.0
 
         for positive in data.positives:
             assert(db.check(positive))
 
         for negative in data.negatives:
             if db.check(negative):
-                false_positives+= 1
+                false_positives += 1
 
-        print("False Negative Rate: " + str(100* false_negatives / len(data.positives)) + "%")
-        print("False Positive Rate: " + str(100* false_positives / len(data.negatives)) + "%")
+        print("Train/dev false Positive Rate: " + str(100* false_positives / len(train_dev_negatives)) + "%")
+
+        false_positives = 0.0
+        for neg in test_negatives:
+            if db.check(neg):
+                false_positives += 1
+
+        
+        print("Test false positive rate: ", str(100* false_positives / len(test_negatives)) + "%")
 
 TestDeepBloom().test_almost_perfect_model()
