be0af45ab1d58cc2034d0fea85da75f2202bef91
Karan Singhal <karan1149@gmail.com>
2018-06-02 21:16:05 -0700

    GRU test for deep bloom and general cleanup

--- lib/test_deep_bloom.py
+++ lib/test_deep_bloom.py
@@ -2,51 +2,72 @@ from DeepBloom import DeepBloom
 from Model import Model
 from AlmostPerfectModel import AlmostPerfectModel
 from PerfectModel import PerfectModel
+from GRUModel import GRUModel
 import json
 import random
 import string
+from utils import * 
 
-class Data(object):
-    def __init__(self, positives, negatives):
-        self.positives = positives
-        self.negatives = negatives
+## Get data to train on
+with open('../data/dataset.json', 'r') as f:
+    dataset = json.load(f)
 
-class TestDeepBloom(object):
-    def test_almost_perfect_model(self):
-        fp_rate = .05
+positives = dataset['positives']
+negatives = dataset['negatives']
 
-        ## Get data to train on
-        with open('../data/dataset.json', 'r') as f:
-        	dataset = json.load(f)
+def test_almost_perfect_model():
+    fp_rate = .05
 
-        positives = dataset['positives']
-        negatives = dataset['negatives']
+    train_dev_negatives = negatives[:int(.90 * len(negatives))]
+    test_negatives = negatives[int(.90 * len(negatives)):]
+    print("Number train, dev", len(train_dev_negatives))
+    print("Number test", len(test_negatives))
+    data = Data(positives, train_dev_negatives)
 
-        train_dev_negatives = negatives[:int(.90 * len(negatives))]
-        test_negatives = negatives[int(.90 * len(negatives)):]
-        print("Number train, dev", len(train_dev_negatives))
-        print("Number test", len(test_negatives))
-        data = Data(positives, train_dev_negatives)
+    # this parameter is not related to fp_rate
+    db = DeepBloom(AlmostPerfectModel(.2), data, fp_rate)
+    
+    for positive in data.positives:
+        assert(db.check(positive))
 
-        # this parameter is not related to fp_rate
-        db = DeepBloom(AlmostPerfectModel(.2), data, fp_rate)
-        false_positives = 0.0
+    false_positives = 0.0
+    for negative in data.negatives:
+        if db.check(negative):
+            false_positives += 1
+    print("Train/dev false Positive Rate: " + str(100* false_positives / len(train_dev_negatives)) + "%")
 
-        for positive in data.positives:
-            assert(db.check(positive))
+    false_positives = 0.0
+    for neg in test_negatives:
+        if db.check(neg):
+            false_positives += 1
+    print("Test false positive rate: ", str(100* false_positives / len(test_negatives)) + "%")
 
-        for negative in data.negatives:
-            if db.check(negative):
-                false_positives += 1
+def test_gru_model():
+    fp_rate = 0.05
 
-        print("Train/dev false Positive Rate: " + str(100* false_positives / len(train_dev_negatives)) + "%")
+    train_dev_negatives = negatives[:int(.90 * len(negatives))]
+    test_negatives = negatives[int(.90 * len(negatives)):]
+    print("Number train, dev", len(train_dev_negatives))
+    print("Number test", len(test_negatives))
+    data = Data(positives, train_dev_negatives)
 
-        false_positives = 0.0
-        for neg in test_negatives:
-            if db.check(neg):
-                false_positives += 1
+    db = DeepBloom(GRUModel('../data/glove.6B.50d-char.txt', 50, 0.001), data, fp_rate)
 
-        
-        print("Test false positive rate: ", str(100* false_positives / len(test_negatives)) + "%")
+    for positive in data.positives:
+        assert(db.check(positive))
 
-TestDeepBloom().test_almost_perfect_model()
+    false_positives = 0.0
+    for negative in data.negatives:
+        if db.check(negative):
+            false_positives += 1
+    print("Train/dev false Positive Rate: " + str(100* false_positives / len(train_dev_negatives)) + "%")
+
+    false_positives = 0.0
+    for neg in test_negatives:
+        if db.check(neg):
+            false_positives += 1
+    print("Test false positive rate: ", str(100* false_positives / len(test_negatives)) + "%")
+
+
+# test_almost_perfect_model()
+test_gru_model()
