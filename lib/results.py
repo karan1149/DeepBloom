f4a2dce1dd5132d57a392be9f97ae69dbd4d372d
Karan Singhal <karan1149@gmail.com>
2018-06-03 18:25:42 -0700

    Try on smaller dataset

--- lib/results.py
+++ lib/results.py
@@ -4,6 +4,7 @@ from DeeperBloom import DeeperBloom
 import json
 from utils import *
 from test_deep_bloom import test_gru_model
+from test_bloom_filter import url_test
 
 if __name__=='__main__':
 	with open('../data/dataset.json', 'r') as f:
@@ -11,6 +12,23 @@ if __name__=='__main__':
 
 	positives = dataset['positives']
 	negatives = dataset['negatives']
+
+	'''
+	Baseline bloom filter 0.01 fp
+	Bits needed 8020489
+	Hash functions needed 6
+	False positives 0.009922375051069045
+
+	Baseline 0.001 fp
+	Bits needed 12030733
+	Hash functions needed 9
+	False positives 0.0010336374778700803
+	'''
+	# url_test(0.01)
+
+	positives = positives[:int(len(positives) * .5)]
+	negatives = negatives[:int(len(negatives) * .5)]
+
 
 	print("First attempt to get results")
 	model = GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=None, maxlen=40, gru_size=16, batch_size=2048, lr=0.005, hidden_size=8)
