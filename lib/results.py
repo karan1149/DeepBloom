9a170adcf74728aaf2417e5c53308f839db8a94f
Karan Singhal <karan1149@gmail.com>
2018-06-03 21:58:53 -0700

    Increase batch size

--- lib/results.py
+++ lib/results.py
@@ -32,10 +32,10 @@ if __name__=='__main__':
 	print("Baseline")
 	url_test(positives, negatives, 0.01)
 	print("First attempt to get results, no data balancing")
-	model = GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=1024, lr=0.005, hidden_size=8, epochs=40)
+	model = GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=40)
 	test_gru_model(positives, negatives, model, train_dev_fraction=0.95, deeper_bloom=False, fp_rate=0.01)
 
 	print("deeper bloom version with k = 2")
-	models = [GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=1024, lr=0.005, hidden_size=8, epochs=40) for _ in range(2)]
+	models = [GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=40) for _ in range(2)]
 	test_gru_model(positives, negatives, models, train_dev_fraction=0.95, deeper_bloom=True, fp_rate=0.01)
 
