4f4f7e53466e580aa692fda983b4726529e4635e
Karan Singhal <karan1149@gmail.com>
2018-06-04 20:49:37 -0700

    Less tests for now

--- lib/results.py
+++ lib/results.py
@@ -31,9 +31,9 @@ if __name__=='__main__':
 	print(len(positives), len(negatives))
 	print("Baseline")
 	url_test(positives, negatives, 0.01)
-	print("First attempt to get results, no data balancing")
-	model = GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60)
-	test_gru_model(positives, negatives, model, train_dev_fraction=0.95, deeper_bloom=False, fp_rate=0.01)
+	# print("First attempt to get results, no data balancing")
+	# model = GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60)
+	# test_gru_model(positives, negatives, model, train_dev_fraction=0.95, deeper_bloom=False, fp_rate=0.01)
 
 	print("deeper bloom version with k = 2")
 	models = [
@@ -42,11 +42,11 @@ if __name__=='__main__':
 		]
 	test_gru_model(positives, negatives, models, train_dev_fraction=0.95, deeper_bloom=True, fp_rate=0.01, fp_fractions=[0.25, 0.25, 0.5])
 
-	print("deeper bloom version with k = 3")
-	models = [
-		GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60), 
-		GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60),
-		GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60)
-		]
-	test_gru_model(positives, negatives, models, train_dev_fraction=0.95, deeper_bloom=True, fp_rate=0.01, fp_fractions=[0.1666, 0.1666, 0.1666, 0.5])
+	# print("deeper bloom version with k = 3")
+	# models = [
+	# 	GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60), 
+	# 	GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60),
+	# 	GRUModel('../data/glove.6B.50d-char.txt', 50, pca_embedding_dim=16, maxlen=40, gru_size=16, batch_size=8192, lr=0.005, hidden_size=8, epochs=60)
+	# 	]
+	# test_gru_model(positives, negatives, models, train_dev_fraction=0.95, deeper_bloom=True, fp_rate=0.01, fp_fractions=[0.1666, 0.1666, 0.1666, 0.5])
 
